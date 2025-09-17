437bac63b1a80c417a2ce82a963c4cc7d7cae0b5
franktinongbe <franktinongbe86@gmail.com>
2025-09-17 12:21:06 +0200

    ajout de conig

--- AJA-229/vite.config.js
+++ AJA-229/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // <-- IMPORTANT pour que Vercel serve correctement les assets
 })
