06274ba632ae054c142a13952dcdf4b5630ba902
franktinongbe <franktinongbe86@gmail.com>
2026-04-08 20:41:00 +0200

    new

--- AJA-229/vite.config.js
+++ AJA-229/vite.config.js
@@ -15,12 +15,12 @@ export default defineConfig({
         theme_color: '#ffffff',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: 'flavicon.ico',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'favicon.ico',
             sizes: '512x512',
             type: 'image/png'
           }
